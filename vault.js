f2ef62476a92ef800d398cc8f851d6a2543a1a49
doug <drcox23@gmail.com>
2018-07-07 16:38:26 -1000

    it works...

--- vault.js
+++ vault.js
@@ -1,4 +1,33 @@
 'use strict';
 module.exports = function() {
   
+    console.log('aloha');
+    const mySecretVault = {};
+    
+    const setValue = function(key,value){
+        if(!key){
+            return null;
+        }else{
+        mySecretVault.globalKey = key;
+        mySecretVault.globalValue = value;
+        }
+    }
+    
+    const getValue = function(setKey){
+        if(!setKey){
+            return null
+        }else if (setKey !== mySecretVault.globalKey){
+            return null;
+        }else if (setKey === mySecretVault.globalKey){
+            return mySecretVault.globalValue;
+        }
+    }
+    
+
+    return {
+        getValue: getValue,
+        setValue: setValue
+    }
+console.log(mySecretVault); 
+    
 };
